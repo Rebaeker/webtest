a840a70a7a329594386a086c5d4fa127433d853d
Tim Ehmann <t-ehmann@gmx.de>
2025-07-17 19:06:21 +0200

    added solid renderer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-
+import solid from '@astrojs/solid-js';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
@@ -7,7 +7,7 @@ export default defineConfig({
   // <-- DIESE ZEILE IST WICHTIG
   // Du kannst hier später weitere Integrationen wie Tailwind CSS hinzufügen
   site: 'https://www.example.com',
-
+  integrations: [solid()],
   vite: {
     plugins: [tailwindcss()]
   }
